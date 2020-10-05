e10d45448579c3f7c239e95239b28d1acb5b7fd1
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-10-05 23:39:14 +0530

    Create jobs model

---
+++ model/jobs.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+// Setup Schema
+const jobsSchema = mongoose.Schema({
+  companyName: {
+    type: String,
+    required: true,
+  },
+  jobTitle: {
+    type: String,
+    required: true,
+  },
+  location: {
+    sublocality_level_1: { type: String },
+    locality: { type: String },
+    administrative_area_level_1: { type: String },
+    administrative_area_level_2: { type: String },
+    country: { type: String, required: true },
+  },
+  jobDescription: {
+    type: String,
+    required: true,
+  },
+  skills: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+});
+
+const Jobs = mongoose.model("Jobs", jobsSchema);
+module.exports = Jobs;

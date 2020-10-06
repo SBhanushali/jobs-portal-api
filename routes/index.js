f24c9ce79d170bf2e2ec6e8a370ef0c325958889
SBhanushali <shivam.n.bhanushali@gmail.com>
2020-10-06 11:53:59 +0530

    Change structure message

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const router = require("express").Router();
 
 // Set default API response
 router.get("/", (req, res) => {
-  res.json({
-    status: "API working",
+  res.status(200).json({
+    success: true,
     message: "Welcome to Job Portal ",
   });
 });
